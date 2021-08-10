7e2f3bc35c62d4bb9aa6d39d5d9a291447b06ef6
Julia <juliasilveira.dsouza@gmail.com>
2021-08-10 14:46:57 -0300

    Início searchScreen

---
+++ ProjetoFichamento/Pesquisar/searchScreenViewController.swift
@@ -0,0 +1,12 @@
+//
+//  searchScreenViewController.swift
+//  ProjetoFichamento
+//
+//  Created by Julia Silveira de Souza on 10/08/21.
+//
+
+import UIKit
+
+struct searchScreenViewController {
+    
+}
