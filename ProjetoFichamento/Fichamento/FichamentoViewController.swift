8724a57fabf450111bd2648086b30815aafe5eb9
joselinch <linchburmann@gmail.com>
2021-08-11 17:51:04 -0300

    Conexão banco e tela de criação de fichamentos.

--- ProjetoFichamento/Fichamento/FichamentoViewController.swift
+++ ProjetoFichamento/Fichamento/FichamentoViewController.swift
@@ -7,20 +7,22 @@
 
 import UIKit
 
-class FichamentoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FichamentoViewController: UIViewController, FoldersModalListViewControllerDelegate {
+    
+    func didSelectedCategory(category: Category) {
+        self.selectedCategory = category
+    }
+    
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    var attachmentItem: [Files]?
-    var selectedAttachment = ""
     var readingStatus = ""
     @IBOutlet var textFieldTitle: UITextField!
     @IBOutlet weak var statusButtonWidth: NSLayoutConstraint!
-    @IBOutlet var searchBarFolder: UISearchBar!
     @IBOutlet weak var  datePicker: UIDatePicker!
     @IBOutlet var userNotes: UITextView!
-    @IBOutlet var tableViewAttachments: UITableView!
     @IBOutlet var textFieldAuthor: UITextField!
     @IBOutlet var textFieldReference: UITextField!
+    var selectedCategory: Category?
     
     //MARK: Reading Status Button
     @IBOutlet var presentStatusButtonOutlet: UIButton!
@@ -41,36 +43,12 @@ class FichamentoViewController: UIViewController, UITableViewDelegate, UITableVi
         self.present(statusSheet, animated: true, completion: nil)
     }
     
-    //MARK: Calendar Picker
-    @IBOutlet var dateButtonOutlet: UIButton!
-    @IBAction func dateButton(_ sender: Any) {
-    }
-    
-    //MARK: Attachment Button
-    @IBOutlet var attachmentButtonOutlet: UIButton!
-    @IBAction func addAttachmentButton(_ sender: Any) {
-        let attachmentSheet = UIAlertController(title: nil, message: "Choose which file you want to attach:", preferredStyle: .actionSheet)
-        
-        //MARK: To-do Ver como selecionar para abrir os files/camera do usuário (tem que pedir permissao)
-        let attachmentOptions = ["File","Camera","Image or Picture"]
-        
-        for item in attachmentOptions {
-            let setAction = UIAlertAction(title: "\(item)", style: .default) { (_) in
-                self.selectedAttachment = item
-                self.attachmentButtonOutlet.setTitle(item, for: .normal)
-            }
-            attachmentSheet.addAction(setAction)
-        }
-        
-        let sheetCancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        attachmentSheet.addAction(sheetCancelButton)
-        self.present(attachmentSheet, animated: true, completion: nil)
-    }
     
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
         let sucessAlert = UIAlertController(title: "Success", message: "Record saved!", preferredStyle: .alert)
 //        let alertOkButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
 //        sucessAlert.addAction(alertOkButton)
+        print(selectedCategory?.name ?? "")
         self.present(sucessAlert, animated: true, completion: nil)
         self.dismiss(animated: true, completion: nil)
         //performSegue(withIdentifier: "", sender: nil)
@@ -82,39 +60,23 @@ class FichamentoViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     override func viewDidLoad() {
-       // tableViewAttachments.dataSource = self
-      //  tableViewAttachments.delegate = self
         presentStatusButtonOutlet.layer.cornerRadius = 6.0
         presentStatusButtonOutlet.layer.borderWidth = 1
         presentStatusButtonOutlet.layer.cornerRadius = 6
         presentStatusButtonOutlet.layer.borderColor = UIColor(red: 0.929, green: 0.439, blue: 0.341, alpha: 1).cgColor
-    //    dateButtonOutlet.layer.cornerRadius = 6.0
-    //    dateButtonOutlet.layer.borderWidth = 1
-    //    dateButtonOutlet.layer.cornerRadius = 6
-    //    dateButtonOutlet.layer.borderColor = UIColor(red: 0.929, green: 0.439, blue: 0.341, alpha: 1).cgColor
-    //    attachmentButtonOutlet.layer.cornerRadius = 6.0
-    //    attachmentButtonOutlet.layer.borderWidth = 1
-     //   attachmentButtonOutlet.layer.cornerRadius = 6
-    //    attachmentButtonOutlet.layer.borderColor = UIColor(red: 0.929, green: 0.439, blue: 0.341, alpha: 1).cgColor
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         statusButtonWidth.constant = view.frame.width - 60 - datePicker.frame.width
     }
     
-    //MARK: Attachments TableView
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if segue.identifier == "modal-folder" {
+            let navController = segue.destination as? UINavigationController
+            let destination = navController?.topViewController as? FoldersModalListViewController
+            destination?.delegate = self
+        }
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "filesCell", for: indexPath) as! AttachmentTableViewCell
-        let attachment = self.attachmentItem![indexPath.row]
-        cell.fileTitle.text = attachment.fileName
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.attachmentItem?.count ?? 0
-    }
 }
---
+++ ProjetoFichamento/Fichamento/FoldersModalListViewController.swift
@@ -0,0 +1,94 @@
+//
+//  FoldersModalListViewController.swift
+//  ProjetoFichamento
+//
+//  Created by José Antônio Linch Burmann on 11/08/21.
+//
+
+import UIKit
+
+protocol FoldersModalListViewControllerDelegate: AnyObject {
+    func didSelectedCategory(category: Category)
+}
+
+class FoldersModalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+
+    weak var delegate: FoldersModalListViewControllerDelegate?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func createButton(_ sender: Any) {
+        let alert = UIAlertController(title: "New Folder", message: "Enter a name for this folder", preferredStyle: .alert)
+        
+        alert.addTextField(){ (textField) in
+            textField.placeholder = "Enter a name"
+        }
+        
+        let alertCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(alertCancel)
+        
+        let alertSave = UIAlertAction(title: "Save", style: .default) { (action) in
+              let textField = alert.textFields![0]
+            
+            let name = textField.text
+            addCategory(name: name ?? "")
+            self.fetchData()
+        }
+        
+        alert.addAction(alertSave)
+        self.present(alert, animated: true, completion: nil)
+    
+    }
+    
+    private var pastas: [Category]?
+    
+    @IBAction func doneAction(_ sender: Any) {
+        navigationController?.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchData()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func fetchData(){
+        pastas = returnCategory()
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pastas?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "list-folder", for: indexPath)
+        
+        cell.textLabel?.text = pastas?[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let pasta = pastas?[indexPath.row] {
+            delegate?.didSelectedCategory(category: pasta)
+        }
+        
+        navigationController?.dismiss(animated: true, completion: nil)
+    }
+
+    
+    
+}
