18ddea0f74ae2314d7f34968cbe12c2a6ac0b9a6
Julia <juliasilveira.dsouza@gmail.com>
2021-08-13 15:09:59 -0300

    Setup Inicial Onboarding

---
+++ ProjetoFichamento/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,99 @@
+//
+//  OnboardingViewController.swift
+//  ProjetoFichamento
+//
+//  Created by Julia Silveira de Souza on 13/08/21.
+//
+
+import UIKit
+
+class OnboardingViewController: UIPageViewController {
+    
+    var pages = [UIPageViewController]()
+    let pageControl = UIPageControl()
+    let initialPage = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    
+    func setup() {
+        dataSource = self
+        delegate = self
+        
+        pageControl.addTarget(self, action: #selector(pageControlTapped(_:)), for: .valueChanged)
+        
+        let page1 = viewController1()
+        let page2 = viewController2()
+        let page3 = viewController3()
+
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
+        
+        setViewControllers([pages[initialPage]], direction: .forward, animated: true, completion: nil)
+    }
+    
+    func style() {
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.currentPageIndicatorTintColor = .black
+        pageControl.pageIndicatorTintColor = .systemGray2
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = initialPage
+    }
+    
+    func layout() {
+        view.addSubview(pageControl)
+    }
+}
+
+extension OnboardingViewController {
+    @objc func pageControlTapped(_ sender: UIPageControl) {
+        setViewControllers([pages[sender.currentPage]], direction: .forward, animated: true, completion: nil)
+    }
+}
+
+extension OnboardingViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore
+        viewController: UIViewController) -> UIViewController? {
+        
+        guard let currentIndex = pages.firstIndex(of: viewController as! UIPageViewController)
+        else { return nil }
+        
+        if currentIndex == 0 {
+            return pages.last
+        } else {
+            return pages[currentIndex -1]
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let currentIndex = pages.firstIndex(of: viewController) else {
+            return nil }
+        
+        if currentIndex < pages.count -1 {
+            return pages[currentIndex + 1]
+        } else {
+            return pages.first
+        }
+    }
+}
+
+extension OnboardingViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        guard let viewControllers = pageViewController.viewControllers else { return }
+        
+        guard currentIndex = pages.firstIndex(of: viewControllers[0]) else { return }
+        
+        pageControl.currentPage = currentIndex
+    }
+}
+
