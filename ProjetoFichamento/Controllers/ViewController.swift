c6dc8ff70c5687359dfc88bff0509ecf267ea8e7
Julia <juliasilveira.dsouza@gmail.com>
2021-08-13 20:00:28 -0300

    update Onboardind e LaunchScreen

--- ProjetoFichamento/Controllers/ViewController.swift
+++ ProjetoFichamento/Controllers/ViewController.swift
@@ -6,7 +6,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+//    override func viewDidLayoutSubviews() {
+//        super .viewDidLayoutSubviews()
+//        if Core.shared.isNewUser() {
+//            //OnboardingViewController
+//        }
+//    }
 }
 
+//class Core {
+//
+//    static let shared = Core()
+//
+//    func isNewUser() -> Bool {
+//        return !UserDefaults.standard.bool(forKey: "isNewUser")
+//    }
+//
+//    func setIsNotNewUser() {
+//        UserDefaults.standard.set(true, forKey: "isNewUser")
+//    }
+//}
--- ProjetoFichamento/Onboarding/Page2.swift
+++ ProjetoFichamento/Onboarding/Page2.swift
@@ -1,5 +1,5 @@
 //
-//  OnboardingViewController.swift
+//  Page2.swift
 //  ProjetoFichamento
 //
 //  Created by Julia Silveira de Souza on 13/08/21.
@@ -7,16 +7,19 @@
 
 import UIKit
 
-class OnboardingViewController: UIViewController {
+class Page2: UIViewController {
     
     let stackView = UIStackView()
     let imageView = UIImageView()
     let titleLabel = UILabel()
+    let startButton = UIButton()
+    
     
     init(imageName: String, titleText: String) {
         super.init(nibName: nil, bundle: nil)
         imageView.image = UIImage(named: imageName)
         titleLabel.text = titleText
+        startButton.title(for: .normal)
     }
     
     required init?(coder: NSCoder) {
@@ -30,27 +33,30 @@ class OnboardingViewController: UIViewController {
     }
 }
 
-extension OnboardingViewController {
+extension Page2 {
     
     func style() {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
-        stackView.alignment = .center
-        //stackView.spacing = 20
+        //stackView.alignment = .center
+        stackView.distribution = .equalCentering
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleToFill
+        imageView.contentMode = .top
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.font = UIFont.preferredFont(forTextStyle: .body)
-        titleLabel.backgroundColor = UIColor(named: "Color1Primary")
+        titleLabel.textColor = UIColor(named: "BackgroundsTertiary")
         
-        view.backgroundColor = UIColor(named: "Color3Tertiary")
+        startButton.backgroundColor = UIColor(named: "Color1Primary")
+        
+        view.backgroundColor = UIColor(named: "Color2Tertiary")
     }
     
     func layout() {
         stackView.addArrangedSubview(imageView)
         stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(startButton)
         
         view.addSubview(stackView)
         
@@ -58,7 +64,9 @@ extension OnboardingViewController {
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             
-            imageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
         ])
     }
 }
+
